325a8472d49c86ea9c9a755d71b60e38d905af6f
Tatiana Milova <TYCR2020@yandex.ru>
2024-09-02 22:00:45 +0300

    work

---
+++ homework8/src/math.js
@@ -0,0 +1,15 @@
+// function add(a, b) {
+//     return a + b;
+//    }
+   
+//    function subtract(a, b) {
+//     return a - b;
+//    }
+   
+//    module.exports = {
+//     add,
+//     subtract
+//    };
+
+module.exports.add = (a, b) => a + b;
+module.exports.subtract = (a, b) => a - b;
---
+++ homework8/src/server.js
@@ -0,0 +1,4 @@
+const math = require('./math');
+
+console.log(math.add(7, 5));
+console.log(math.subtract(10, 3));
